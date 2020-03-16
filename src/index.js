4b22dfacdc04eb17e719b2035d36a60963611ed5
SofiBretz <sofiabarrantes.gamboa@gmail.com>
2020-03-16 16:02:20 -0600

    start the listscontainer and the render function on the app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('hello');
+const listsContainer = document.querySelector('[data-lists]')
+
+let lists = ['name', 'todo']
+
+function render() {
+    <li class="list-name">Work</li>
+    clearElement(listsContainer)
+    lists.forEach(list => {
+        const listElement = document.createElement('li')
+        listElement.classList.add("list-name")
+        listElement.innerText = list
+        listsContainer.appendChild(listElement)
+    })
+}
+
+function clearElement(element) {
+
+}
