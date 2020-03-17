9e20182c3557cce174048b87b6d1a1592473bd4f
SofiBretz <sofiabarrantes.gamboa@gmail.com>
2020-03-16 18:19:12 -0600

    added a function to remove an element, webpack and eslint fixed

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
-const listsContainer = document.querySelector('[data-lists]')
+const listsContainer = document.querySelector("[data-lists]");
 
-let lists = ['name', 'todo']
-
-function render() {
-    <li class="list-name">Work</li>
-    clearElement(listsContainer)
-    lists.forEach(list => {
-        const listElement = document.createElement('li')
-        listElement.classList.add("list-name")
-        listElement.innerText = list
-        listsContainer.appendChild(listElement)
-    })
-}
+let lists = ["name", "todo"];
 
 function clearElement(element) {
-
+  while (element.firstChild) {
+    element.remove(element.firstChild);
+  }
 }
+
+function render() {
+  clearElement(listsContainer);
+  lists.forEach(list => {
+    const listElement = document.createElement("li");
+    listElement.classList.add("list-name");
+    listElement.innerText = list;
+    listsContainer.appendChild(listElement);
+  });
+}
+
+render();
