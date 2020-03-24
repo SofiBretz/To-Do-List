ee808c0a638e32a888dc4a1c64bf2c6ed206cf57
SofiBretz <sofiabarrantes.gamboa@gmail.com>
2020-03-24 15:14:41 -0600

    fixing the linters

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
-const path = require("path");
-const TerserPlugin = require("terser-webpack-plugin");
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist")
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   optimization: {
     minimize: true,
-    minimizer: [new TerserPlugin()]
+    minimizer: [new TerserPlugin()],
   },
   module: {
     rules: [
@@ -17,20 +17,20 @@ module.exports = {
         test: / \.js$ /,
         exclude: /(node_modules)/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"]
-          }
-        }
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
       {
         test: /\.css$/,
-        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
       },
       {
         test: /\.(jpg|jpeg|png|gif)$/,
-        use: [{ loader: "url-loader" }]
-      }
-    ]
-  }
+        use: [{ loader: 'url-loader' }],
+      },
+    ],
+  },
 };
